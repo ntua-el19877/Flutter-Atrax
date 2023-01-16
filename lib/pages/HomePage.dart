06137ccf4b63665f47cf41193bc8c166e578e291
ntua-el19877 <el19877@mail.ntua.gr>
2023-01-16 18:59:32 +0200

    test

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -10,6 +10,7 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.red,
       appBar: AppBar(
         title: const Text('Atrax Main Page'),
       ),
